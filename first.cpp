e3c07bff463b9fddab9306f0574613800f6b4d1b
Veepin_Kumar <veepin8459@gmail.com>
2026-03-09 13:01:06 +0530

    Add A1/A2/A3 and B classes; instantiate B
    Add classes A1, A2 and A3 with constructors that print messages; A3 is inherited virtually. Add class B that inherits A1, A2 and virtual A3 and logs its construction. Update main() to construct a B instance instead of printing Hello World!. Also add a new binary file 'first' to the repository (new executable).

--- first.cpp
+++ first.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
 using namespace std;
+
+class A1{
+    public:
+        A1(){
+            cout<<"A1 constructor called!"<<endl;
+        }
+};
+class A2{
+    public:
+        A2(){
+            cout<<"A2 constructor called!"<<endl;
+        }
+};
+
+class A3{
+    public:
+
+    A3(){
+        cout<<"A3 constructor called!"<<endl;
+    }
+};
+class B : public A1, public A2, virtual A3{
+    public:
+        B(){
+            cout<<"B constructor called!"<<endl;
+        }
+};
+
 int main(){
-    cout<<"Hello World!"<<endl;
+    class B b;
+    
+    
     return 0;
 }
